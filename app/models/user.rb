b90b1b96177ea3321a89daab16001a1095a1efff
deguchi0527 <ryodegu0527@gmail.com>
2021-04-07 14:49:11 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
   validates :nickname, presence: true
   validates :birth_date, presence: true
 
-  validates :password, format: {with: VALID_PASSWORD_REGEX}
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  with_options presence: true,  format: {with: VALID_NAME_REGEX} do
+  with_options presence: true, format: { with: VALID_NAME_REGEX } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: {with: VALID_NAME_KANA_REGEX} do
+  with_options presence: true, format: { with: VALID_NAME_KANA_REGEX } do
     validates :first_name_kana
     validates :last_name_kana
   end
