b53e79bd8f10e2bf22d124bbdb2cc88221e90d0c
deguchi0527 <ryodegu0527@gmail.com>
2021-04-07 21:01:28 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :name, :info, :image, presence: true
+  
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validatable :scheduled_delivery_id
+  end
+
+  validates :price, presence: true, inclusion: {in: (300..9_999_999)}, format: {with: /\A[0-9]+\z/ }
 end
