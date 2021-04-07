cfc46e43bf84ac6c97cd2900eed9305a3d09fc96
deguchi0527 <ryodegu0527@gmail.com>
2021-04-07 14:42:57 +0900

    モデルの単体テストを実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    gimei = Gimei.name
+    email {Faker::Internet.free_email}
+    password {"1a" + Faker::Internet.password(min_length: 4)}
+    password_confirmation {password}
+    nickname {Faker::Name.initials}
+    last_name {gimei.last.kanji}
+    first_name {gimei.first.kanji}
+    last_name_kana {gimei.last.katakana}
+    first_name_kana {gimei.first.katakana}
+    birth_date {Faker::Date.in_date_period}
+  end
+end
