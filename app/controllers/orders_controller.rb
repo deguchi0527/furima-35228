e2eadab55a1546705d383542fd9ec45b407f527a
deguchi0527 <ryodegu0527@gmail.com>
2021-04-10 11:32:15 +0900

    モデル、コントローラー、ルーティングの設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,10 @@
+class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    binding.pry
+  end
+end
