e23194e33899ac92bf714b3c30cf75e5f72ca1ff
deguchi0527 <ryodegu0527@gmail.com>
2021-04-17 17:04:52 +0900

    アカウント編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :updata]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
     @items = @user.items
@@ -9,11 +9,20 @@ class UsersController < ApplicationController
   end
   
   def update
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
   
   private
   
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_cofirmation)
   end
 end
