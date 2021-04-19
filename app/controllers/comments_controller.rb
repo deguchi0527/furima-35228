651e487062ebb730f8a11c5e0a6e85bb44a596df
deguchi0527 <ryodegu0527@gmail.com>
2021-04-19 23:28:29 +0900

    Rubocopの実行

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,14 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      ActionCable.server.broadcast "comment_channel", content: @comment, nickname: @comment.user.nickname
+      ActionCable.server.broadcast 'comment_channel', content: @comment, nickname: @comment.user.nickname
     else
       redirect_to item_path(@comment.item.id)
     end
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,29 +5,28 @@ RSpec.describe Comment, type: :model do
     @comment = FactoryBot.build(:comment)
   end
 
-  describe "コメントの保存" do
-    context "コメント投稿が保存できる場合" do
-      it "textが正しく入力できていれば保存できる" do
+  describe 'コメントの保存' do
+    context 'コメント投稿が保存できる場合' do
+      it 'textが正しく入力できていれば保存できる' do
         expect(@comment).to be_valid
       end
     end
-    context "コメント投稿が保存できない場合" do
-      it "textが空では保存できない" do
-        @comment.text = ""
+    context 'コメント投稿が保存できない場合' do
+      it 'textが空では保存できない' do
+        @comment.text = ''
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("コメントを入力してください")
+        expect(@comment.errors.full_messages).to include('コメントを入力してください')
       end
-      it "userが紐付いていないと保存できない" do
+      it 'userが紐付いていないと保存できない' do
         @comment.user = nil
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("Userを入力してください")
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
       end
-      it "itemが紐付いていないと保存できない" do
+      it 'itemが紐付いていないと保存できない' do
         @comment.item = nil
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("Itemを入力してください")
+        expect(@comment.errors.full_messages).to include('Itemを入力してください')
       end
     end
-
   end
 end
